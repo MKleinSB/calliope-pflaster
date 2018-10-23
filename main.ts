fecfd5ee829141d275734d4e98d6147f8343fb1a
Werja <Werner.J@rmatz.de>
2018-10-23 17:17:54 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace Math {
+    export function idiv(x: number, y: number) { return x / y }
+    export function round(x: number) { return x }
+}
